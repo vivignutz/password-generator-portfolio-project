b93f6e9d23290254a83dccee66c8892ce1ebc8e3
vivignutz <vgnutzmann@gmail.com>
2022-11-11 17:19:26 +0000

    Add script for the password generator and started to debbuging

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+//** take the input id="slider" */
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#value");
+let generatedPassword = document.querySelector("#generated-password");
+
+let containerPassword = document.querySelector("#container-password");
+ 
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?@€$&#*ß";
+let newPassword = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    password.innerHTML = pass;
+    containerPassword.classList.remove("hide");
+    newPassword = pass; 
+}
+
+function copyPassword() {
+    alert("Password successfull copied!"); 
+    navigator.clipboard.writeText(newPassword);
+}
 
